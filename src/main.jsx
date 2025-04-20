0c3390814f1fdd00d2691f636e70eb2ef0af4f44
guidobigode <Gui_vanusi@yahoo.com.br>
2025-04-20 08:56:23 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SimuladosApp from './SimuladosApp.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SimuladosApp from "./SimuladosApp.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SimuladosApp />
   </React.StrictMode>
-)
+);
